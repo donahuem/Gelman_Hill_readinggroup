9586e656bbb3430bdb6db546d6a43fd0f3ffb952
fishymary <mdono@hawaii.edu>
2016-04-15 16:46:37 -1000

    updating with changes made during class

--- Ch.16/ch16_examples_mary.R
+++ Ch.16/ch16_examples_mary.R
@@ -5,13 +5,13 @@ library(rjags)
 library(coda)
 
 # set up the data ---------------------------------------------------------
-srrs2 <- read.table("srrs2.dat",header=T,sep=",")      # read in the data
+srrs2 <- read.table("srrs2.dat",header=T,sep=",")      # read in the data, folder: data -> radon
 mn <- srrs2$state=="MN"                                # subset for Minnesota
 radon <- srrs2$activity[mn]                            # subset radon levels for MN
 y <- log(ifelse(radon==0,.1,radon))                    # our response will be log radon because we are not assuming multiplactive effects, round 0s to 0.1 before taking the log
 n <- length(radon)                                     # sample size
 x <- srrs2$floor[mn]                                   # our dependent variable will be floor, 0 for basement, 1 for first floor
-srrs2.fips <- srrs2$stfips*1000 + srrs2$cntyfips
+# srrs2.fips <- srrs2$stfips*1000 + srrs2$cntyfips
 county.name <- as.vector(srrs2$county[mn])             # subset county for MN
 uniq.name <- unique(county.name)                       # make a list of only unique names (length=85)
 J <- length(uniq.name)                                 # number of groups J
@@ -29,6 +29,12 @@ display(lm.pooled)
 lm.unpooled.0 <- lm(y~x+factor(county))
 display(lm.unpooled.0)
 lm.unpooled <- lm(y~x+factor(county)-1)                # removes constant term, each county has own intercept
+display(lm.unpooled)
+
+# multilevel in lmer
+library(lme4)
+mult.lm <- lmer(y~x+(1|factor(county)))
+display(mult.lm)
 
 # multilevel regression with JAGS  ----------------------------------------
 
@@ -37,7 +43,7 @@ cat("model{
   for(i in 1:n){
     y.hat[i] <- a[county[i]] + b*x[i]                  # data model (e.g. 16.1a)
     y[i] ~ dnorm(y.hat[i], tau.y)                      # y[i] distributed normal with mean y.hat and precision tau.y (precision = 1/variance)
-  }
+  }                                                    # loop ends here that defines likelihood
   b ~ dnorm(0,0.0001)                                  # prior for b
   tau.y <- pow(sigma.y, -2)                            # tau.y is equal to 1/sigma.y squared
   sigma.y ~ dunif(0,100)                               # prior for sigma.y
@@ -51,32 +57,39 @@ cat("model{
 
 # call JAGS from R
 # 2. Compile the model, check for undefined parameters, and get ready to collect posterior samples
-radon.data <- list(y=y,x=x,n=n,county=county,J=J)      # define data in a list
-radon.inits <- list(a=rnorm(J),b=rnorm(1)
-                    ,mu.a=rnorm(1), sigma.y=runif(1),  # set up list of initial values to start the sampler
-                    sigma.a=runif(1))
+radon.data <- list(y=y,x=x,n=n,county=county,J=J)      # define data in a list "knowns"
+# set up list of initial values to start the sampler, number of lists should be the number chains
+radon.inits <- list(list(a=rnorm(J),b=rnorm(1)
+                         ,mu.a=rnorm(1), sigma.y=runif(1),  
+                         sigma.a=runif(1)),
+                    list(a=rnorm(J),b=rnorm(1)
+                         ,mu.a=rnorm(1), sigma.y=runif(1),  
+                         sigma.a=runif(1)),
+                    list(a=rnorm(J),b=rnorm(1)
+                         ,mu.a=rnorm(1), sigma.y=runif(1),  
+                         sigma.a=runif(1)))
 
 jags.mod <- jags.model(file="jags_multilevel",         # construct the jags model object in R
                        data=radon.data,
-                       inits=radon.inits,
-                       n.chains=3,                     # 3 MCMC chains
+                       inits=radon.inits,              # can comment this line if want random initials
+                       n.chains=length(radon.inits),   # 3 MCMC chains
                        n.adapt=100)                    # number of runs to toss for JAGS to figure out tuning parameter for jump size
 
 # 3. Initialize the model
-# load.module("dic")                                   # load the DIC module to monitor deviance
+load.module("dic")                                     # load the DIC module to monitor deviance
 params <- c("deviance","b","mu.a","sigma.y","sigma.a") # make a list of paramters for JAGS to monitor
 samps <- coda.samples(jags.mod,params
-                      ,n.iter=2000                     # run 2000 iterations
-                      ,thin=10)                        # sample every 10 iterations
+                      ,n.iter=2000)                    # run 2000 iterations
 plot(samps)                                            # trace plots to assess mixing
 
-st <- start(samps)                                     # save for later
+st <- start(samps)                                     # save start and end points for later
 en <- end(samps)
 
 # 4. Adapt and burn in
 burn.in <- 1000
 update(jags.mod, n.iter=burn.in)                       # update to the model to start after burn in
-samps <- coda.samples(jags.mod,params,n.iter=2000,thin=10)    # run samples again
+samps <- coda.samples(jags.mod,params,n.iter=2000      # run samples again
+                      ,thin=10)                        # sample every 10th run
 
 # 5. Monitor
 plot(samps)
